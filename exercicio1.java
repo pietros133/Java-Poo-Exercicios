d3157d802a5f273aa91b2c6fa96b6cff924f5d44
Pietro Santos Miranda <161655720+pietros133@users.noreply.github.com>
2024-07-29 16:40:03 -0300

    Add files via upload

---
+++ exercicio1.java
@@ -0,0 +1,23 @@
+package teste_processamento;
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // Conversor de Temperaturas
+		 double C, K, F, Re, Ra;
+	        C = 2.43;
+
+	        F = C * 1.8 + 32;
+	        K = C + 273.15;
+	        Ra = C * 1.8 + 32 + 459.67;
+	        Re = C * 0.8;
+
+	        System.out.println("A temperatura em Fahrenheit é: " + F);
+	        System.out.println("A temperatura em Kelvin é: " + K);
+	        System.out.println("A temperatura em Reaumur é: " + Ra);
+	        System.out.println("A temperatura em Rankine é: " + Re);
+	        	
+	}
+
+}
